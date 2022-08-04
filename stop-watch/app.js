e772401df42ad186e0b67e9dec6e70947a263b49
Ben Wagner <bmwbenswagner@gmail.com>
2022-08-04 15:18:23 -0500

    Implement stopwatch function

--- stop-watch/app.js
+++ stop-watch/app.js
@@ -1,2 +1,20 @@
-const startStopBtn = document.querySelector('#startStopBtn');
-const resetBtn = document.querySelector('#resetBtn');
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function stopWatch() {
+  seconds++;
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+
+    if (minutes / 60 === 1) {
+        minutes = 0;
+        hours++;
+    }
+  }
+}
